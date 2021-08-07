7f146bc7d0759b525653c99b1c7de00afcfdb6c0
Bruno Aggierni <bruno.aggierni@gmail.com>
2021-08-07 18:00:26 +0100

    Hamburger Nav Bar created

---
+++ public/script.js
@@ -0,0 +1,8 @@
+// BURGER MENU 
+
+const menuButton = document.querySelector('#mobile-menu');
+
+menuButton.addEventListener('click' , e => {
+  const menu = document.querySelector('.mobile-links');
+  menu.classList.toggle('hidden')
+});
